03289ae853c25f88afa3bb3383e57ea0312b2b32
ABHINAV GHOSH <35833124+logicinfinite@users.noreply.github.com>
2019-05-12 23:57:45 +0530

    Add files via upload

---
+++ pm1.py
@@ -0,0 +1,39 @@
+import mysql.connector
+import time
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="1234",
+    database="pm"
+)
+print("THIS IS logicinfinite PASSWORD VAULT")
+
+n = int(input("how many entries you want to register"))
+while n != 0:
+
+    username = input("enter username")
+    password = input("enter password")
+    mycursor = mydb.cursor()
+    sqlformula = "INSERT INTO studs (name,age) VALUES (%s,%s)"
+    val = (username, password)
+    mycursor.execute(sqlformula, val)
+    mydb.commit()
+    n -= 1
+
+
+u = input('enter username for which you need to see the password')
+sqlformula1 = "SELECT age FROM studs WHERE name=%s"
+val1 = (u,)
+mycursor.execute(sqlformula1, val1)
+
+myresult = mycursor.fetchall()
+print("password for entered username is ")
+for i in myresult:
+
+    print(i)
+
+time.sleep(5)
+i = input("press 0 to exit")
+if i == 0:
+    exit(0)
